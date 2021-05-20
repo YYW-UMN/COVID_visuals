7a4af7b16ba11867697d392dd822c1956503d9fa
Yuanyuan Wang <38868191+YYW-UMN@users.noreply.github.com>
2021-05-20 14:50:55 -0400

    Update join_dedup_meta_nextstrain.R

--- join_dedup_meta_nextstrain.R
+++ join_dedup_meta_nextstrain.R
@@ -2,7 +2,7 @@ join_dedup_meta_nextstrain <- function(save_rds = TRUE){
   
   dedup_primary <- tbl(impala, 'deduplication_primary_auto') %>% as.data.frame()
   analytics_metadata <- tbl(impala, 'analytics_metadata') %>% as.data.frame()
-  nextclade <- tbl(impala, 'nextclade') %>% as.data.frame() %>% select(nt_id, clade)
+ # nextclade <- tbl(impala, 'nextclade') %>% as.data.frame() %>% select(nt_id, clade) # not using the nextclade table
   
   cat("======Two tables imported as data.frame======")
   
@@ -19,8 +19,8 @@ join_dedup_meta_nextstrain <- function(save_rds = TRUE){
   meta <- analytics_metadata %>% 
     inner_join(dedup_primary, by = "primary_virus_name") 
   
-  meta <- meta %>% 
-    left_join(nextclade, by = c("primary_nt_id.x" = "nt_id"))
+ # meta <- meta %>% 
+ #   left_join(nextclade, by = c("primary_nt_id.x" = "nt_id"))
   
   cat("======Final table produced======")
   
