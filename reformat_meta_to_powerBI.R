e4bd96c864870836746f16255b6dab53279c1978
Yuanyuan Wang <38868191+YYW-UMN@users.noreply.github.com>
2021-05-23 19:09:32 -0400

    Update reformat_meta_to_powerBI.R

--- reformat_meta_to_powerBI.R
+++ reformat_meta_to_powerBI.R
@@ -1,6 +1,5 @@
-reformat_meta_to_powerBI <- function(meta){
+reformat_meta_to_powerBI <- function(meta, vclist){
   reformat_meta <- meta %>% 
-  # filter(country_iso_code != "US") %>%
     mutate(host = primary_host.x) %>%
     filter(host == "Human") %>%
     rename(strain = primary_virus_name) %>%
@@ -14,7 +13,6 @@ reformat_meta_to_powerBI <- function(meta){
     mutate(length = primary_sequence_length) %>%
     mutate(pango_lineage = lineage) %>%
     mutate(mutations = spike_mutations) %>%
-    mutate(originating_lab = covv_orig_lab) %>% 
     select(strain, 
            nt_id,
            gisaid_epi_isl, 
@@ -25,14 +23,14 @@ reformat_meta_to_powerBI <- function(meta){
            location,
            length,
            pango_lineage,
-           mutations,
-           originating_lab)
+           mutations)
   reformat_meta <- reformat_meta %>% 
     mutate(country_code = if_else(
       country == "Kosovo","XKX", 
       if_else(country == "Eswatini", "SWZ", 
               countrycode(sourcevar = country, origin = "country.name", destination = "iso3c"))))
   
+  # Get the WHO countries
   gitdata.dir <- "https://raw.githubusercontent.com/CDCgov/ITF_Power_BI/master/itf_dashboard/output/"
   who_countries <- read.csv(paste0(gitdata.dir,"country_data.csv"), encoding="UTF-8") %>%
     rename(country_code = iso3code) %>%
@@ -43,12 +41,17 @@ reformat_meta_to_powerBI <- function(meta){
     filter(!duplicated(strain)) %>%
     rename(country = country.x) %>%
     select(-c(country.y, nt_id, length)) %>%
-    rows_update(tibble(strain = "SARS-CoV-2/Human/USA/AK-CDC-2-4242656/1988", date = "2021-04-01"))
+    rows_update(tibble(strain = "SARS-CoV-2/Human/USA/AK-CDC-2-4242656/1988", date = "2021-04-01")) # fix the typo of 1988
 
+  variant_label <- vclist %>% filter(label == "VOI" | label == "VOC") 
+  
   new_meta <- new_meta %>%
-    mutate(date = replace(date, date == "2051-04-05","2021-04-05"))
+    mutate(date = replace(date, date == "2051-04-05","2021-04-05")) %>%
+    filter(pango_lineage != "NA" & pango_lineage != "none") %>%
+    left_join(variant_label, by = "pango_lineage") %>%
+    rename(type = label) %>% # rename label to type
+    replace_na(list(type = "Other"))  # replace NA in type with "Other" for the figure
   
   return(new_meta)
 }
-
 
